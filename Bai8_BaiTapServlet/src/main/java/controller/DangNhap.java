2973a2d9d14dc66b1131d8af5e9f89f535c29a37
lenhattung <lenhattung@gmail.com>
2022-10-05 19:12:34 +0200

    2022.10.05
    2022.10.05

---
+++ Bai8_BaiTapServlet/src/main/java/controller/DangNhap.java
@@ -0,0 +1,65 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DangNhap
+ */
+@WebServlet("/dang-nhap")
+public class DangNhap extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public DangNhap() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Set character
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String tenDangNhap = request.getParameter("tenDangNhap");
+		String matKhau = request.getParameter("matKhau");
+		
+		String url ="";
+		if (tenDangNhap.equalsIgnoreCase("tung")&&matKhau.equals("123")) {
+			//response.getWriter().append("Chúc mừng bạn đã đăng nhập thành công!");
+			// url = "/success.jsp";
+			response.sendRedirect("success.jsp");
+			
+		}else {
+			//response.getWriter().append("Đăng nhập thất bại!");
+			//url = "/error.jsp";
+			response.sendRedirect("error.jsp");
+		}
+		
+		RequestDispatcher rd = getServletContext().getRequestDispatcher(url);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ Bai8_BaiTapServlet/src/main/webapp/login.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="dang-nhap" method="POST">
+		<h1> Đăng Nhập </h1>
+		Tên người dùng:<input type="text" name="tenDangNhap" /> <br/>  
+		Mật khẩu: <input type="password" name="matKhau" /> <br/>  <br/>
+		<input type="submit" value="Đăng nhập"/>
+	</form>
+</body>
+</html>
